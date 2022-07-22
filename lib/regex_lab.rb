423caa64c71ec05a039a5b2b61a210240d9fa813
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-07-22 16:53:24 -0700

    Completed lab

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/\A[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z]/) ? (text.match(/\.$/) ? true : false) : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.0-9]*$/) ? true :false
 end
